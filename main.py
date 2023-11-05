13f54746642bb0326e6ca46f2b4ea6fba30de35b
buonhobo <naturytropik@gmail.com>
2023-11-05 22:44:23 +0100

    ignore

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import sys
 from topology.wizard import configure_topology
 
 
-
 if __name__ == "__main__":
-    if len(sys.argv)!=3:
+    if len(sys.argv) != 3:
         print(f"Usage: {sys.argv[0]} <config directory> <target directory>")
         exit(1)
 
@@ -13,4 +12,3 @@ if __name__ == "__main__":
     target = Path(sys.argv[2])
 
     configure_topology(config, target)
-
